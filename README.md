1cccff3b93cd5b29608bd8b4f9ed54d36f4e1055
Haroen Viaene <hello@haroen.me>
2016-10-26 12:29:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Counter Counter
+
+The elusive and nice <code>counter counter</code> by <a href="https://bullg.it">bullgit</a>
+
+## Usage
+
+Install `counter-counter` via `npm`.
+
+Import the script (or add it via a tag).
+
+```js
+import CounterCounter from 'counter-counter';
+
+const c = new CounterCounter('count!', '#demo'); // arguments are optional
+```
+
+The default selector for the container is `#countercounter`
+
+## Demo
+
+A demo can be see on [bullg.it/counter-counter](bullg.it/counter-counter)!
+
+## License
+
+Apache 2.0
+
+Initially made by Haroen Viaene
---
+++ counter-counter.js
@@ -0,0 +1,39 @@
+class CounterCounter {
+  constructor(selector = '#countercounter', text = 'Count!') {
+    this.container = document.querySelector(selector);
+    this.text = text;
+    this.addStyle();
+    this.count(this.container);
+  }
+
+  count(container) {
+    const c = document.createElement('div');
+    const button = document.createElement('button');
+
+    c.classList.add('countercounter');
+
+    if (typeof container.dataset.count !== 'undefined') {
+      container.dataset.count = parseInt(container.dataset.count, 10) + 1;
+    }
+    c.dataset.count = 0;
+    button.innerText = this.text;
+
+    button.addEventListener('click', _ => {
+      this.count(c);
+    });
+
+    c.appendChild(button);
+    container.appendChild(c);
+  }
+
+  addStyle() {
+    const rules = `
+.countercounter:before {
+  content: attr(data-count);
+}
+`;
+    const style = document.createElement('style');
+    style.innerHTML = rules;
+    this.container.appendChild(style);
+  }
+}
